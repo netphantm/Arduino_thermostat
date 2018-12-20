086c8d920779c7af924cdd006fe5e7523f358d52
Hugo <mail@hugo.ro>
2018-12-20 11:31:08 +0100

    init

--- settings.php
+++ settings.php
@@ -1,72 +1,24 @@
 <?php
-
-if( !empty($_GET["IP"]) && !empty($_GET["SHA1"]) && !empty($_GET["host"]) && !empty($_GET["httpsPort"]) && !empty($_GET["interval"]) && !empty($_GET["temp_min"]) && !empty($_GET["temp_max"]) ) {
-  $IP = $_GET["IP"];
-  $SHA1 = $_GET["SHA1"];
-  $host = $_GET["host"];
-  $httpsPort = $_GET["httpsPort"];
-  $interval = $_GET["interval"];
-  $temp_min = $_GET["temp_min"];
-  $temp_max = $_GET["temp_max"];
-  header('Location: '.$_GET["IP"]."update?SHA1=".$SHA1."&host=".$host."&httpsPort=".$httpsPort."&interval=".$interval."&temp_min=".$temp_min."&temp_max=".$temp_max);
-  exit;
-}
-
-function x509_fingerprint($pem,$hash='sha1')
-{
-  $pem = preg_replace('/\-+BEGIN CERTIFICATE\-+/','',$pem);
-  $pem = preg_replace('/\-+END CERTIFICATE\-+/','',$pem);
-  $pem = str_replace( array("\n","\r"), '', trim($pem));
-  return strtoupper(hash($hash,base64_decode($pem)));
-}
-
-$contextOptions = array(
-  'ssl' => array(
-    'verify_peer' => false,
-    'verify_peer_name' => false,
-    'disable_compression' => true,
-    'capture_peer_cert' => true,
-    #'allow_self_signed' => true,
-  )
-);
-
-$g = stream_context_create($contextOptions);
-$r = stream_socket_client("ssl://localhost:443", $errno, $errstr, 20, STREAM_CLIENT_CONNECT, $g);
-$cont = stream_context_get_params($r);
-// Store remote certificate as a string
-openssl_x509_export($cont['options']['ssl']['peer_certificate'], $x509_cert);
-
-$pem = preg_replace('/\-+BEGIN CERTIFICATE\-+/','',$x509_cert);
-$pem = preg_replace('/\-+END CERTIFICATE\-+/','',$pem);
-
-$SHA1 = x509_fingerprint($pem,$hash='sha1');
-$SHA1 = wordwrap($SHA1 , 2 , ':' , true );
-$host = "temperature.hugo.ro";
-$httpsPort = "443";
-$interval = 120000;
-$temp_min = 6;
-$temp_max = 12;
-
 print("<html><head>\n");
 print("</head><body>\n");
 print("<link rel=\"shortcut icon\" href=\"https://www.hugo.ro/favicon.ico\"/>\n");
-print("<title>Clamps Thermostat IoT Settings</title>\n");
+print("<title>Grrrl Thermostat IoT Settings</title>\n");
 print("<style>\n.content { background-color: lightcyan; width: 1000px; margin: auto; }</style>\n");
 print("</head><body>\n<div class=\"content\">\n");
-print("<form action=\"\" method=\"GET\">\n");
+print("<form action=\"http://" + $IP + /update\" method=\"POST\">\n");
 print("<div align=\"center\"><h1>ESP8266-12E DHT11 - Thermostat IoT - Settings</h1></div>\n");
-print("<div align=\"center\"><table style=\"width:950px;\"><tr><td>\n");
+print("<div align=\"center\"><table style=\"width:950px;\"><tr><td>");
 
-print("Hostname = <select name=\"IP\">\n");#onchange=\"this.form.action=this.value
-#print(" <option value=\"\">Select...</option>\n");
-print(" <option value=\"http://192.168.178.104/\">Clamps</option>\n");
-print("</select><br>\n");
-print("Certificate Fingerprint SHA1 = <input type='text' name='SHA1' maxlength=59 size=40 value=$SHA1><br>\n");
-print("Host = <input type='text' name='host' size=11 value=$host>\n");
-print("Port = <input type='text' name='httpsPort' size=1 value=$httpsPort><br>\n");
-print("Refresh interval = <input type='text' name='interval' size=2 value=$interval><br>\n");
-print("Temperature MIN = <input type='text' name='temp_min' size=1 value=$temp_min><br>\n");
-print("Temperature MAX = <input type='text' name='temp_max' size=1 value=$temp_min><br>\n");
+print("IP = <input type='select' name='IP'>");
+print(" <option value="">Select...</option>");
+print(" <option value="192.168.178.104"></option>");
+print("</select>");
+print("Certificate Fingerprint SHA1 = <input type='text' name='SHA1' maxlength=59 size=55><br>");
+print("Host = <input type='text' name='host' size=15>");
+print("Port = <input type='text' name='httpsPort' size=5><br>");
+print("Refresh interval = <input type='interval' name='SHA1' size=5><br>");
+print("Temperature max = <input type='interval' name='temp_max' size=5><br>");
+print("Temperature min = <input type='interval' name='temp_min' size=5><br>");
 print("<input type='submit' value='Submit' >\n");
-print("</form></body>");
-?>
+
+print("</body>");

37b1e87785ba184cd668d710d02af8353a78db94
netphantm <mail@hugo.ro>
2018-12-27 11:44:09 +0100

    return-epochTime

--- logtemp.php
+++ logtemp.php
@@ -5,6 +5,7 @@ if(!empty($_GET["uptime"]) && !empty($_GET["status"]) && !empty($_GET["temperatu
   $epochTime = ($epoch * 1000);
   $niceTime = (new DateTime("$epochTime[0]"))->format('Y-m-d H:i:s');
   $csvData = array($_GET["uptime"],$_GET["status"],$_GET["temperature"],$_GET["temp_min"],$_GET["temp_max"],$epochTime,$_GET["heater"],$_GET["manual"]);
+  print($epochTime);
 
   $fcsv = fopen("/var/www/temp/temp-log-".$_GET["IP"].".csv","a"); 
   if($fcsv) {

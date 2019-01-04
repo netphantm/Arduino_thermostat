6dc861047a042b5c4d4a2f04b40e6af909d64f85
netphantm <mail@hugo.ro>
2019-01-04 09:05:21 +0100

    reply also with niceTime

--- logtemp.php
+++ logtemp.php
@@ -3,9 +3,9 @@
 if(!empty($_GET["status"]) && !empty($_GET["temperature"]) && !empty($_GET["IP"])) {
   $epoch = array_sum( explode( ' ' , microtime() ) );
   $epochTime = ($epoch * 1000);
-  //$niceTime = (new DateTime("$epochTime[0]"))->format('Y-m-d H:i:s');
+  $niceTime = (new DateTime("$epochTime[0]"))->format('Y-m-d H:i:s');
   $csvData = array($_GET["status"],$_GET["temperature"],$_GET["temp_min"],$_GET["temp_max"],$epochTime,$_GET["heater"],$_GET["manual"]);
-  print($epochTime);
+  print($epochTime.", ".$niceTime);
 
   $fcsv = fopen("/var/www/temp/temp-log-".$_GET["IP"].".csv","a"); 
   if($fcsv) {
